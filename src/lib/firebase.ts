fa0f1d98b69387be608f3c39d065d961993c07a9
Stephen Kumar <rsksk2002@gmail.com>
2025-09-26 08:36:03 +0000

    File changes

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -5,12 +5,12 @@ import { getFirestore, collection, addDoc, query, where, orderBy, limit, getDocs
 import type { Calculation } from '@/types';
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  "projectId": "studio-7820799130-4a199",
+  "appId": "1:525995542255:web:da83178b99ab81ca8afd1a",
+  "apiKey": "AIzaSyBmy0iPgZsuwpGST8FeaGDLDvjUoE1-HMw",
+  "authDomain": "studio-7820799130-4a199.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "525995542255"
 };
 
 // Initialize Firebase
